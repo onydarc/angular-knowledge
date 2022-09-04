7c705632f3ec4f17b27564e8d0d12b0f75bcf84a
onydarc <onydarc@gmail.com>
2022-09-04 00:04:30 -0600

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent  {
+
+ heroes:string[]=['SpiderMan','IronMan','Hulk'];
+ heroeBorrado:string='';
+
+ borrarHeroe(){
+  this.heroeBorrado = this.heroes.shift()||'';
+ }
+
+}
